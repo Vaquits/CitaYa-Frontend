d1f98b0ea92d2ae00badf1df2ef92a83308c9d88
Anderson <andersonj.urbinav@gmail.com>
2022-05-25 01:54:28 -0500

    feat: Add specialist dashboard layout

---
+++ src/app/pages/specialists/specialist-dashboard/specialist-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-specialist-dashboard',
+  templateUrl: './specialist-dashboard.component.html',
+  styleUrls: ['./specialist-dashboard.component.scss']
+})
+export class SpecialistDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
